e1745578909fe3f2864b3fa2ca9b1617e4fa6a7d
Anita Chau <anitachau0@gmail.com>
2017-12-02 23:13:29 -0500

    add website pictures

---
+++ assets/js/scroll.js
@@ -0,0 +1,26 @@
+$(function() {
+	// register scrollspy to the body
+	$('body').scrollspy({ target: '#main-navbar',offset: 58});
+
+	// add smooth scrolling on all links inside the navbar
+	$('#main-navbar a').on('click', function(event) {
+		// make sure this.hash has a value before overriding degault behaviour
+		if (this.hash !== "") {
+			// prevent default anchor click behaviour
+			event.preventDefault();
+
+			// store hash
+			var hash = this.hash;
+
+			// Use jQuery.animate() to add smooth page scrollspy
+			// use 800 ms time frame to perform scroll
+			$('html,body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function() {
+				// add hash (#) to URL when done scrolling 
+				// this will make our URL bar match where we are on the page
+				window.location.hash = hash;
+			});
+		}
+	});
+});
